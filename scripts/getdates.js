62fce4209b74770d113e2ec483a4752a57285953
SnakeLover23 <snakey232323@gmail.com>
2025-11-12 09:19:39 -0700

    made WDD 131 home page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const today = new Date()
+const currentyear = document.querySelector("#currentyear")
+currentyear.innerHTML = `${today.getFullYear()}`
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
