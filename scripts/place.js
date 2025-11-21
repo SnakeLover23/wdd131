c082d3cf284a576ac3d57222921d7e6b462d7acb
SnakeLover23 <snakey232323@gmail.com>
2025-11-21 10:27:24 -0700

    finished place project

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,19 @@
+let windspeed = 7.2;
+let temperature = 70.8;
+const windchill_function = function(v, t) {
+    if (temperature <= 50 && windspeed > 3) {
+        let chill = 35.74 + (0.6215 * t) - (35.75 * v ** 0.16) + (0.4275 * t * v ** 0.16);
+        return `${chill.toFixed(2)} °F`;
+    }
+    else {
+        return "N/A";
+    }
+}
+let windchill = windchill_function(windspeed, temperature);
+
+const windchill_output = document.querySelector("#windchill")
+windchill_output.innerHTML = `${windchill}`
+
 
 
 
