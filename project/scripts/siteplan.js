2139038751baec307d9caf98f6ab89addc04e28a
SnakeLover23 <snakey232323@gmail.com>
2025-12-01 10:57:40 -0700

    finished site plan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+const today = new Date()
+const currentyear = document.querySelector("#currentyear")
+currentyear.innerHTML = `${today.getFullYear()}`
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
