febfbb1fa9e7155507d20b62d2cf40bb17945c64
SnakeLover23 <snakey232323@gmail.com>
2025-12-10 16:05:14 -0700

    finished my final project

---
+++ project/scripts/favorites.js
@@ -0,0 +1,40 @@
+const form = document.querySelector(".favz");
+
+localStorage.clear();
+let data = localStorage.getItem("favorites");
+let favorites = JSON.parse(localStorage.getItem("favorites"));
+
+
+form.addEventListener("submit", function() {
+    const type = document.querySelector("#type")
+    const text = type.value;
+    if (data) {
+        favorites.push(text);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    } else {
+        favorites = [text];
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    };
+});
+
+function listFavorites() {
+    if (favorites !== null) {
+        favorites.forEach(favorite => {
+            let li = document.createElement("li");
+            li.textContent = favorite;
+            document.querySelector(".yourfavs").appendChild(li);
+        });
+    } else {
+        let li = document.createElement("li");
+        li.textContent = "You don't have any favorites.";
+        document.querySelector(".yourfavs").appendChild(li);
+    };
+};
+
+listFavorites();
+
+const today = new Date();
+const currentyear = document.querySelector("#currentyear");
+currentyear.innerHTML = `${today.getFullYear()}`;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
